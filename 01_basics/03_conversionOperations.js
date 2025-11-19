4f448848b95e8a0a9033205f13683e1cbf1eb4fa
Pipefitter08 <arsalan.anwer9050@gmail.com>
2025-11-19 03:55:52 +0000

    Learning conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score = false
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "Arsalan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let some = 33
+let stringNumber = String(some)
+console.log(typeof stringNumber);
