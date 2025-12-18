aa39932074343469c323a210be67e37481bf39f1
Pipefitter08 <arsalan.anwer9050@gmail.com>
2025-12-18 05:45:15 +0000

    Learning operators

---
+++ 01_basics/logical.js
@@ -0,0 +1,14 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+let isLoggedIn = true
+let isPaid = false
+console.log(isLoggedIn && isPaid); // both should be true
+let isEmail = true
+let isGoogleUser = false
+
+console.log(isEmail || isGoogleUser) // anyone can be true
+
+
+ 
