8efd433f51b39325acc488b4ef46923b010ad7bf
Pipefitter08 <arsalan.anwer9050@gmail.com>
2025-12-18 01:08:16 +0000

    Learn how to print

---
+++ 01_basics/hello.js
@@ -0,0 +1,5 @@
+// console.log("Hello, World");
+console.log("Hello"); // gives you new line
+process.stdout.write("Tea\n") // does not give you new line
+console.table({Country: "USA",State: "Minnesota"})
+console.warn({Code: "Red"})
