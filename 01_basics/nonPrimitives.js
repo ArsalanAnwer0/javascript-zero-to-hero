5dca5d492fcd4ec2aa1716d14a3eba0d1bbbfdce
Pipefitter08 <arsalan.anwer9050@gmail.com>
2025-12-20 20:13:47 +0000

    Learning primitives and non Primtives

---
+++ 01_basics/nonPrimitives.js
@@ -0,0 +1,18 @@
+// object
+// array
+// function
+
+let user = {name: "Arsalan",age:21}
+let numbers = [1,2,3]
+function greet(){
+    console.log("Hello")
+}
+
+console.log("Playing")
+let user1 = {name: "Ali"}
+let user2 = user1
+user2.name = "Ahmed"
+console.log(user1.name);
+
+
+
---
+++ 01_basics/primitives.js
@@ -0,0 +1,28 @@
+/*
+string
+number
+boolean
+undefined
+null
+symbol
+bigint
+*/
+
+
+let myName = "Arsalan" // string
+let age = 21 // number
+let isLoggedIn = true // boolean
+let score // undefined
+let empty = null; // null
+
+console.log("Playing");
+let a = 10
+let b = a
+
+b = 20
+
+console.log(a)
+console.log(b)
+
+// a and b are separate copies
+// Changing b does NOT affect a
