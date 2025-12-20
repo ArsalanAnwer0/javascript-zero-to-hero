de884ea84965203a09c6d78263b0909e6df9d7fd
Pipefitter08 <arsalan.anwer9050@gmail.com>
2025-12-20 19:49:52 +0000

    Basics of conditionals

---
+++ 01_basics/conditionals.js
@@ -0,0 +1,13 @@
+// Learning conditionals
+let age = 11;
+if(age<10){
+    console.log("Age is ",age, ", and it is less than 10")
+} else{
+    console.log("Age is ",age," and Age is more than 10")
+}
+
+let loggedIn = true;
+
+if (loggedIn){
+    console.log("Welcome")
+}
