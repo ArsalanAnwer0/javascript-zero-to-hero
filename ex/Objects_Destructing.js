0bd1b6b9b6c60123205de637ce3849330c8169cf
Pipefitter08 <arsalan.anwer9050@gmail.com>
2025-12-21 02:26:33 +0000

    Learning objects and destructing

---
+++ ex/Objects_Destructing.js
@@ -0,0 +1,16 @@
+let user = {
+    myName: "Arsalan",
+    age: 21,
+    email: "arsalan.anwer9050@gmail.com",
+    address: {
+        city: "St. Cloud",
+        country: "USA"
+    }
+}
+
+let {myName,age} = user
+console.log(age);
+console.log(myName)
+let {email} = user
+console.log(email)
+let {city} = user
